5c26bbdbf4962e25467971fbdb079f098286e207
Shahvalad <shahvaladayvazov@gmail.com>
2024-02-28 13:03:13 +0400

    initial commit

---
+++ src/Services/Category/CategoryApi/Controllers/CategoriesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "Smartphones", "Watches" });
+        }
+    }
+}
---
+++ src/Services/Product/ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "Iphone 15", "Rolex" });
+        }
+    }
+}
